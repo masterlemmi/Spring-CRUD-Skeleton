ce6a42756e36ef9bd537870e95d0886c7c76643e
lemzki <lemzki@gmail.com>
2018-12-08 13:19:03 +0800

    first commit

---
+++ src/main/java/com/lemzki/tools/familydb/FamilyDbApplication.java
@@ -0,0 +1,33 @@
+package com.lemzki.tools.familydb;
+
+
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.context.annotation.Bean;
+
+import java.util.stream.Stream;
+
+
+
+@SpringBootApplication
+public class FamilyDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyDbApplication.class, args);
+	}
+
+	@Bean
+	ApplicationRunner init(CarRepository repository) {
+		return args -> {
+			Stream.of("Ferrari", "Jaguar", "Porsche", "Lamborghini", "Bugatti",
+					"AMC Gremlin", "Triumph Stag", "Ford Pinto", "Yugo GV").forEach(name -> {
+				Car car = new Car();
+				car.setName(name);
+				repository.save(car);
+			});
+			repository.findAll().forEach(System.out::println);
+		};
+	}
+}
